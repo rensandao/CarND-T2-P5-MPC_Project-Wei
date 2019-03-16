dd79725e2b5cf0fc8fb43ebe0e1d4682f397431d
rensandao <38071371+rensandao@users.noreply.github.com>
2019-03-16 23:44:54 +0800

    Update MPC.cpp

--- src/MPC.cpp
+++ src/MPC.cpp
@@ -33,7 +33,7 @@ size_t delta_start = epsi_start + N;
 size_t a_start = delta_start + N - 1;
 
 class FG_eval {
- public:
+public:
   // Fitted polynomial coefficients
   Eigen::VectorXd coeffs;
   FG_eval(Eigen::VectorXd coeffs) { this->coeffs = coeffs; }
